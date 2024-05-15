43ec57ab4f758a0bc3e681cb5dcf50e50caecdc3
unknown <eldergsilva@gmail.com>
2024-05-15 13:50:06 -0300

    instanciado express

--- src/server.js
+++ src/server.js
@@ -1,15 +1,6 @@
-import http, { Server } from "http";
+import app from "./app.js";
 const PORT = 3000;
-const rotas  = {
-    "/":"Curso de Express API",
-    "/livros":"Entrei na rota Livros",
-    "/autores":"Entrei na rota Autores"
-};
-const server  = http.createServer((req,res)=>{
-    res.writeHead(200, {"content-Type":"text/pain"})
-    res.end(rotas[req.url])
-});
 
-server.listen(PORT,()=>{
+app.listen(PORT,()=>{
     console.log("servidor escutando");
 })
